dcb0fd00824d171407065a8b5e8b26dea45a3577
Fe1Fan <feifan00x@gmail.com>
2023-03-02 17:52:44 +0800

    config

--- jlite-data/src/main/java/io/xka/data/Main.java
+++
@@ -1,7 +0,0 @@
-package io.xka.jlite;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- jlite-data/src/main/java/io/xka/data/iData.java
+++
@@ -1,4 +0,0 @@
-package io.xka.data;
-
-public interface iData {
-}
